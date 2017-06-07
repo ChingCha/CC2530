e096121543381ce981f0a335d0884eacfe960e0e
Yi-Ting Chen <s15115148@stu.edu.tw>
2017-06-07 23:51:45 +0800

    06_07
    Learn Class

---
+++ Programming/ObjectforRFSend.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class RFsend{
+	public:
+		int a;
+		int appsend();
+};
+
+
+int 
--- Programming/classwithballhead.h
+++ Programming/classwithballhead.h
@@ -21,15 +21,3 @@ private:
     double _radius; // 半徑 
     string _name; // 名稱 
 };
-
-/*Class四階段：
-	-	類別(Class)：
-		#	類別成員可以是資料(Data)、變數(variable)、函數(Function)、建構子(Constructor)
-		#	歸屬於類別的OXOX，皆稱為成員(member)變數(variable)、函數(Function)
-		#	至於建構子(Constructor)屬於特別的成員函數，用來建立該類別物件的專屬函數。
-		#	
-		
-
-
-
-*/
