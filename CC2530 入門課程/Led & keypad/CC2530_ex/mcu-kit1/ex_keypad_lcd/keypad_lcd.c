f519f90d96030672900f1d2966b9c5711d486b13
Yi-Ting Chen <s15115148@stu.edu.tw>
2017-08-21 00:02:15 +0800

    CC2530
    + Test I/o
    + 思考如何使個別Button擁有兩種狀態

--- CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/keypad_lcd.c
+++ CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/keypad_lcd.c
@@ -1,73 +1,77 @@
 #include "ioCC2530.h"
 
-#define LED1 P1_0
-#define LED2 P1_1
-#define LED3 P1_2
-#define LED4 P1_3
-#define LED5 P1_4
-#define LED6 P1_5
-#define LED7 P1_6
-#define LED8 P1_7
-
-
-void delay(unsigned int t)
+#define LED5 P0_7
+#define LED6 P0_6
+#define SW1 P0_5
+#define SW2 P0_4
+unsigned char Stat_key = 0;   //按???全局?量
+/*===================延?函?=========================*/
+void Delay(unsigned int t)
 {
   while(t--);
 }
-
-void LEDRunning()
+/*================端口初始化函?======================*/
+void InitPort()
 {
-    LED1 = 1;
-    delay(60000);
-    LED2 = 1;
-    delay(60000);
-	LED3 = 1;
-	delay(60000);
-	LED4 = 1;
-	delay(60000);
-	LED5 = 1;
-	delay(60000);
-	LED6 = 1;
-	delay(60000);
-	LED7 = 1;
-	delay(60000);
-	LED8 = 1;
-	delay(60000);
-	
-	LED1 = 0;
-    delay(60000);
-    LED2 = 0;
-    delay(60000);
-	LED3 = 0;
-	delay(60000);
-	LED4 = 0;
-	delay(60000);
-	LED5 = 0;
-	delay(60000);
-	LED6 = 0;
-	delay(60000);
-	LED7 = 0;
-	delay(60000);
-	LED8 = 0;
-	delay(60000);
+  P0SEL &= ~0xC0;         //?P1_3和P1_4?置?通用I/O端口功能
+  P0DIR |= 0xC0;          //?P1_3和P1_4的端口??方式?置??出
+  P0SEL &= ~0x30;         //?P0_1?置?通用I/O端口功能
+  P0DIR &= ~0x30;         //?P0_1的端口??方式?置??入
+
+  LED5 = 0;               //上?的?候，LED5不亮
+  LED6 = 0;               //上?的?候，LED6不亮
 }
-
-void main(void)
+/*=================按??描函?=======================*/
+void ScanKeys()
 {
-   P1SEL &= ~0xFF;	//通用I/O Port
-   P1DIR |= 0xFF;	//設置為輸出
   
-   P1_0 = 0;
-   P1_1 = 0;
-   P1_2 = 0;
-   P1_3 = 0;
-   P1_4 = 0;
-   P1_5 = 0;
-   P1_6 = 0;
-   P1_7 = 0;
+  if(SW1 == 0)
+  {                       //??SW1有低?平信?
+    Delay(100);           //按?去抖?
+    if(SW1 == 0)		//确?是有按??作
+    {                      
+      while(SW1 == 0);    //等待按?1松?
+	  
+      //判?按?1按下之前，LED?是打??是??的？
+      if((Stat_key & 0x01) == 0x01)
+      {
+        Stat_key &= ~0x01;  
+        LED5 = 0;
+      }
+      else{
+        Stat_key |= 0x01;
+        LED5 = 1;
+      } 
+    }
+  }
   
-   while(1)
-   {
-    LEDRunning();
-   }
+  
+  if(SW2 == 0)
+  {                       //??SW2有低?平信?
+    Delay(100);           //按?去抖?
+    if(SW2 == 0)
+    {                     //确?是有按??作
+      while(SW2 == 0);    //等待按?2松?
+      if((Stat_key & 0x02) == 0x02)
+      {
+        Stat_key &= ~0x02;
+        LED6 = 0;
+      }
+      else{
+        Stat_key |= 0x02;
+        LED6 = 1;
+      } 
+    }
+  }
+  
+}
+/*=====================主函?=========================*/
+void main()
+{
+  InitPort();
+  while(1)
+  {
+    ScanKeys();
+  }
+  
 }
