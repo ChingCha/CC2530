0a0abccf1623b63a28e91375359d003bb4deada1
Eating <s15115148@stu.edu.tw>
2017-08-21 16:31:09 +0800

    CC2530 入門2
    + Debug Sucess
    + 一個按鈕兩種狀態

--- CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/一個按鈕兩種狀態.c
+++ CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/一個按鈕兩種狀態.c
@@ -4,7 +4,8 @@
 #define LED6 P0_6
 #define SW1 P0_5
 #define SW2 P0_4
-unsigned char Stat_key = 0;   
+
+unsigned char Stat_key = 0;   //按鈕狀態的變數
 
 void Delay(unsigned int t)
 {
@@ -13,10 +14,10 @@ void Delay(unsigned int t)
 
 void InitPort()
 {
-  P0SEL &= ~0xC0;         
-  P0DIR |= 0xC0;         
-  P0SEL &= ~0x30;        
-  P0DIR &= ~0x30;         
+  P0SEL &= ~0xC0;	//將P0_6、P0_7設置為通用I/O         
+  P0DIR |= 0xC0;    //將P0_6、P0_7設置為輸出     
+  P0SEL &= ~0x30;   //將P0_4、5設置為通用I/O     
+  P0DIR &= ~0x30;   //將P0_4、5設置為輸入      
 
   LED5 = 0;               
   LED6 = 0;               
@@ -24,14 +25,14 @@ void InitPort()
 
 void ScanKeys()
 {
-  if(SW1 == 1)
+  if(SW1 == 1)		//發現按鈕有高電位
   {                       
-    Delay(100);          
-    if(SW1 == 1)		
+    Delay(100);		//防止彈跳現象          
+    if(SW1 == 1)	//確認是按下按鈕	
     {                      
-      while(SW1 == 1);    
+      while(SW1 == 1);    //等待按鈕鬆開
 	  
-      
+	  //判斷按下按鈕前，LED是ON還是OFF
       if((Stat_key & 0x01) == 0x01)
       {
         Stat_key &= ~0x01;  
