d774f9341fb4b666ec00c887c24630e445a42f12
Eating <s15115148@stu.edu.tw>
2017-09-21 22:47:38 +0800

    Servo
    + 8051 Version fair
    + Change to Timer3&4

---
+++ CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/Servo/PWM A3(Servo).h
@@ -0,0 +1,96 @@
+#include "ioCC2530.h"
+
+#define LED5 P1_0
+
+void set_main_clock();
+void PortInit();
+void T1Init();
+void Delay(unsigned int t);
+
+void main()
+{
+
+
+	//各工作週期陣列
+    //int dutycycle[11]={0xF7,0xE1,0xC8,0xAF,0x96,0x7D,0x64,0x4B,0x32,0x19,0x0A};
+	
+	PortInit();
+
+	set_main_clock();
+
+	T1Init();
+	
+
+	//T1CC2H = 0x00;
+
+	//T1CC2L = dutycycle[1];
+
+	
+		
+
+}
+
+void Delay(unsigned int t){
+	while(t--);
+}
+
+void PortInit()
+{
+    P0SEL &= ~0x00;		//P1_0設置為通用I/O
+    P0DIR |= 0x01; 		//P1_0設置為輸出
+	LED5 = 0;			//LED初始狀態
+}
+
+
+void T1Init()
+{
+    
+    //Timer通道?置
+    P1SEL |= 0x01;              //Timer1通道2映射至P1_0，功能選擇
+    PERCFG |= 0x40;             //備用位置2，?明信息
+    P2SEL &= ~0x10;             //相對於Timer4，Timer1優先
+    P2DIR |= 0xC0;              //定?器通道2-3具有第一優先順序
+    P1DIR |= 0x01;				//P1_0為輸出
+    
+    //Timer模式?置
+    T1CTL = 0x02;               //250KHZ不分頻，模模式
+    
+    //根據Table7-1，P1_0必須裝Timer1通道2進行比較
+	
+    T1CCTL2 = 0x1C;             //比較相等為1，計數器回0則清零
+	
+    //裝Timer通道0初值
+    T1CC0H = 0x09;
+    T1CC0L = 0xC4;              //PWM信號週期?1ms，頻率為1KHZ
+	
+	
+	
+	
+	
+	//?Timer通道2比?值
+    T1CC2H = 0x04;
+    T1CC2L = 0xE2; 	//1%的正工作週期
+    //T1CC2L = 0xE1; 	//10%的正工作週期
+    //T1CC2L = 0xC8; 	//20%的正工作週期
+    //T1CC2L = 0xAF; 	//30%的正工作週期
+    //T1CC2L = 0x96; 	//40%的正工作週期
+    //T1CC2L = 0xC4; 		//50%的正工作週期
+    //T1CC2L = 0x64; 	//60%的正工作週期
+    //T1CC2L = 0x4B; 	//70%的正工作週期
+    //T1CC2L = 0x32; 	//80%的正工作週期
+    //T1CC2L = 0x19; 	//90%的正工作週期
+    //T1CC2L = 0x0A; //99%的正工作週期
+	
+    //T1CC2L = 0x01; //?置通道2比?寄存器初值
+	
+}
+
+void set_main_clock()
+{ 
+	CLKCONCMD |= 0X40;			//選擇16MHZ RCOSC為系統時鐘源
+	while(!(CLKCONSTA & 0X40)); //等待時鐘穩定
+	CLKCONCMD &=~0XF8;			//選擇32MHz為主時鐘
+	
+	CLKCONCMD |= 0x38;          //Timer標記輸出為250kHz
+}
+
---
+++ CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/Servo/Servo 8051.c
@@ -0,0 +1,125 @@
+#include "ioCC2530.h"
+
+#define PWM P1_0
+#define S1	P0_3
+#define S2	P0_4
+
+char defjump1=0;	//除彈跳變數1(one)
+char defjump2=0;	//除彈跳變數2(zero)
+unsigned char timeout=0;	//PWM信號輸出時間長度
+unsigned char key=0xff;		//鍵值
+unsigned char keyTemp=0xff;	//未除彈跳鍵值
+unsigned char keyData=0xff;	//已除彈跳鍵值
+unsigned char triwave=0;	//三角波
+unsigned char degree45=(200-11);	//45度
+unsigned char degree135=(200-19);//135度
+
+void InitPort();
+void KeyScan(void);
+void InitTimer1();
+
+
+void main(){
+	
+	InitTimer1();	//Timer1初始化
+	while(1){
+		KeyScan();	//按鍵掃描
+		if(keyData!=0xFF){	//按任何鍵?
+			key=keyData;	//儲存鍵值至Key變數中
+			keyData=0xff;	//清除鍵值
+			timeout=0;		//清除timeout
+							//輸出PWM信號
+		}
+		if(S1 == 1){		//如果按S1
+			if(triwave>=degree45)	//輸出Duty Cycle為1.1ms的PWM
+				PWM = 1;
+			else
+				PWM = 0;
+		}
+		if(S2 == 1){
+			if(triwave>=degree135)	//輸出Duty Cycle為1.9ms的PWM
+				PWM = 1;
+			else
+				PWM = 0;
+		}
+		
+	}
+	
+	
+}
+
+void InitPort(){
+	
+	//P0_3、4設置為通用I/O & 輸入
+	P0SEL &= ~0x18;
+	P0DIR &= ~0x18;
+	//P1_0設置為通用I/O & 輸出
+	P1SEL &= ~0x01;		//P1_0設置為通用I/O
+    P1DIR |= 0x01; 		//P1_0設置為輸出
+	
+} 
+
+void KeyScan(void){
+	
+	unsigned char keyStatus;	//按鍵狀態
+	keyStatus = S1;				//讀取S1狀態
+	keyStatus = S2;				//讀取S2狀態
+	if(KeyStatus!=0){			//按任何鍵?
+		
+		defjump1 = 0;
+		if(keyTemp!=keyStatus){	//與上次鍵值不同
+			keyTemp = keyStatus;	//儲存未除彈跳值
+			defjump2 = 1;		//開始除彈跳
+		}else{
+			if(defjump2<5){
+				defjump2+=1;	//defjump2加1
+				if(defjump2==5)	//已完全除彈跳?
+					keyData = keyTemp;	//儲存已儲彈跳值
+			}
+		}	
+	}
+	defjump1+=1;
+	if(defjump1==5){
+		
+		defjump2=0;		//重新掃描按鍵
+		keyTemp = 0xff;	//清除鍵值
+		keyData = 0xff;	//清除鍵值
+	}
+	
+}
+
+void InitTimer1(){
+	
+	PERCFG |= 0x40; 		// set timer_1 I/O位置为2
+	T1CC0L = (256-100); 	//PWM duty cycle  周期
+    T1CC0H = (256-100);
+    
+    T1CC2L = (65536-5000)/256;	//PWM signal period 占空比
+    T1CC2H = (65536-5000)%256;
+    
+    T1CCTL0 |= 0x04;	//開啟定時器1通道0的輸出比較模式
+	
+	IEN1 |= 0x02;	//致能Timer1中斷
+	EA = 1;			//致能總中斷
+	
+    T1CTL |= 0x0f;  // 128KHz分頻，up-down mode(三角波)
+					// Timer1 Control：T1CTL
+	
+}
+
+#pragma vector = T1_VECTOR		
+__interrupt void Timer1_Service(){
+	if(triwave<200)
+		triwave++;
+	else
+		triwave=0;
+	
+	T1CC2L = (65536-5000)/256;	//PWM signal period 占空比
+    T1CC2H = (65536-5000)%256;
+	
+	timeout++;
+	
+	if(timeout>180){
+		timeout = 0;
+	}
+}
