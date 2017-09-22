e8b34c43c05774016ad02f66374fcb86147a9266
Eating <s15115148@stu.edu.tw>
2017-09-22 13:32:28 +0800

    Servo
    + 8051 Version Error
    + CC2530 Timer1 Fair

--- CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/Servo/Servo 8051.c
+++ CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/Servo/Servo 8051.c
@@ -21,6 +21,8 @@ void InitTimer1();
 
 void main(){
 	
+	//KeyScan();
+	InitPort();		//Port初始化
 	InitTimer1();	//Timer1初始化
 	while(1){
 		KeyScan();	//按鍵掃描
@@ -28,7 +30,7 @@ void main(){
 			key=keyData;	//儲存鍵值至Key變數中
 			keyData=0xff;	//清除鍵值
 			timeout=0;		//清除timeout
-							//輸出PWM信號
+			T1CTL = 0x0f;	//輸出PWM信號
 		}
 		if(S1 == 1){		//如果按S1
 			if(triwave>=degree45)	//輸出Duty Cycle為1.1ms的PWM
@@ -50,6 +52,7 @@ void main(){
 
 void InitPort(){
 	
+	PERCFG |= 0x40; 	// set timer_1 I/O位置为2
 	//P0_3、4設置為通用I/O & 輸入
 	P0SEL &= ~0x18;
 	P0DIR &= ~0x18;
@@ -64,7 +67,7 @@ void KeyScan(void){
 	unsigned char keyStatus;	//按鍵狀態
 	keyStatus = S1;				//讀取S1狀態
 	keyStatus = S2;				//讀取S2狀態
-	if(KeyStatus!=0){			//按任何鍵?
+	if(keyStatus!=0){			//按任何鍵?
 		
 		defjump1 = 0;
 		if(keyTemp!=keyStatus){	//與上次鍵值不同
@@ -90,25 +93,28 @@ void KeyScan(void){
 
 void InitTimer1(){
 	
-	PERCFG |= 0x40; 		// set timer_1 I/O位置为2
+	EA = 1;				//致能總中斷
+	IEN1 |= 0x02;		//致能Timer1中斷
+	
+	T1CTL = 0x00;			//1分頻，停止運行
+	T1CTL = 0x0e;  			// 128KHz分頻，up-down mode(三角波)
+	
+	T1CCTL0 |= 0x04;		//開啟定時器1通道0的輸出比較模式
+	
 	T1CC0L = (256-100); 	//PWM duty cycle  周期
     T1CC0H = (256-100);
     
     T1CC2L = (65536-5000)/256;	//PWM signal period 占空比
     T1CC2H = (65536-5000)%256;
     
-    T1CCTL0 |= 0x04;	//開啟定時器1通道0的輸出比較模式
-	
-	IEN1 |= 0x02;	//致能Timer1中斷
-	EA = 1;			//致能總中斷
-	
-    T1CTL |= 0x0f;  // 128KHz分頻，up-down mode(三角波)
-					// Timer1 Control：T1CTL
-	
+
+	IRCON &= ~0x02;		//為了先清除Timer1對應的中斷標誌位
+
 }
 
 #pragma vector = T1_VECTOR		
 __interrupt void Timer1_Service(){
+	T1STAT &= ~0x01;	//清除定時器1通道0中斷標誌
 	if(triwave<200)
 		triwave++;
 	else
@@ -121,5 +127,6 @@ __interrupt void Timer1_Service(){
 	
 	if(timeout>180){
 		timeout = 0;
+		T1CTL = 0x00;
 	}
 }
