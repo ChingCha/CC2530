c18526253779518b1477bac93aafe7ff2b27ee7b
Eating <s15115148@stu.edu.tw>
2017-10-08 15:53:21 +0800

    Test
    + UART Test

--- CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/keypad_lcd.c
+++ CC2530 入門課程/Led & keypad/CC2530_ex/mcu-kit1/ex_keypad_lcd/keypad_lcd.c
@@ -1,146 +1,88 @@
 #include "ioCC2530.h"
 
-#define LED1 P1_3
-#define LED2 P1_4
+#define LED5 P1_0
 
-unsigned char DataRecieve;		//讀取緩衝區資料的變數
-unsigned char Flag = 0;			//接收指令標誌的變數
-
-
-void Init_Port();		//LED Port 初始化函數
-void set_main_clock();	//設置主時鐘
-void Init_UART0();		//序列埠0的初始化函數
-void UR0SendByte(unsigned char data);	//UR0發送字元函數
-void UR0SendString(unsigned char *str);	//UR0發送字串函數
-void ExecuteTheOrder();					//執行上位機指令
-
+void set_main_clock();
+void PortInit();
+void T1Init();
+void Delay(unsigned int t);
 
 void main()
 {
-  Init_Port();	
-  set_main_clock();
-  Init_UART0();
-  
-  while(1){
-	 UR0SendString("ABC"); 
-  }
-  
-  
-  
-  while(1)
-  {
-    if(Flag == 1)      //是否收到上位機指令?
-    {
-      ExecuteTheOrder();    //解析並運行指令
-    }
-  }
-  
-}
-
-void Init_UART0(){
-	
-	//對應的引腳為外設功能
-
-	PERCFG = 0x00;	//串口0的引腳映射到位置1，即P0_2、3
-	P0SEL = 0x0C;	//將P0_2、3 Port 設置成外設功能
-	P2DIR &= ~0x3F;	//P0外設優先級USART0最高
-	
-	U0BAUD = 59;	//16MHz的系統時鐘產生9600BPS鮑率
-	U0GCR = 9;
-	
-	U0UCR |= 0x80;	//禁止流控，8bit數據，清除緩衝器
-	U0CSR |= 0x80;	//選擇UART模式(7)，致能接收器(6)
-	
-	UTX0IF = 0;		//清除TX發送中斷標誌
-	
-	//UART2.c外加
-	
-	U0CSR |=0X40;			//致能UART0 接收
-	IEN0 |=0X04;			//致能UART0 接收中斷
-	EA=1;					//開啟總中斷
-
-}
 
 
-void Init_Port(){
+	//各工作週期陣列
+    //int dutycycle[11]={0xF7,0xE1,0xC8,0xAF,0x96,0x7D,0x64,0x4B,0x32,0x19,0x0A};
 	
-	P1SEL &= ~0x18;		//將P1_4、5設置為通用I/O
-	P1DIR |= 0x18;		//將P1_4、5 Port 設置為輸出
-	LED1 = 0;
-	LED2 = 0;
-	
-}
+	PortInit();
 
+	set_main_clock();
 
-//數據接收中斷服務函數
-#pragma vector = URX0_VECTOR		
-__interrupt void UR0_Recieve_Service(){
+	T1Init();
 	
-	URX0IF = 0;				//清除RX接收中斷標誌
-	DataRecieve = U0DBUF;	//將數據從接收緩衝區讀出
-	Flag = 1;				//設置接收指令標誌
-}
 
+	//T1CC2H = 0x00;
 
-void UR0SendByte(unsigned char data){
-	
-	U0DBUF = data;			//將要發送的1字節數據寫入U0DBUF
-	while(!UTX0IF);			//等待TX中斷標誌，即數據發送完成
-	UTX0IF = 0;				//等待TX中斷標誌，準備下一次發送
-	
-}
+	//T1CC2L = dutycycle[1];
 
-
-void UR0SendString(unsigned char *str){
 	
-	while(*str != '\0'){	//發送一個字串
-		UR0SendByte(*str++);//逐一發送字串的字元
-	}
-	
-}
-
-
-void ExecuteTheOrder(){
-	
-	Flag = 0;	//清除接收指令標誌
-	
-	/*
-	if(DataRecieveTime==1){
-		Buzzer = 1
-		if(Button==1){
-			Buzzer = 0
-			Running1()
-		}
-	else
-		Buzzer = 0
-		Running0()
-	}
-	*/
-	
-	
-	
-	switch(DataRecieve){
 		
-		case 0x01:
-			LED1 = 1;
-			UR0SendString("The LED1 is Open!");
-		break;
-		
-		case 0x02:
-			LED1 = 0;
-			UR0SendString("The LED1 is Closed!");
-		break;
-		
-		case 0x03:
-			LED2 = 1;
-			UR0SendString("The LED2 is Open!");
-		break;
-		
-		case 0x04:
-			LED2 = 0;
-			UR0SendString("The LED2 is Closed!");
-		break;
-	}
+
+}
+
+void Delay(unsigned int t){
+	while(t--);
+}
+
+void PortInit()
+{
+    P1SEL &= ~0x00;		//P1_0設置為通用I/O
+    P1DIR |= 0x01; 		//P1_0設置為輸出
+	LED5 = 0;			//LED初始狀態
+}
+
+
+void T1Init()
+{
+    
+    //Timer通道?置
+    P1SEL |= 0x01;              //Timer1通道2映射至P1_0，功能選擇
+    PERCFG |= 0x40;             //備用位置2，?明信息
+    P2SEL &= ~0x10;             //相對於Timer4，Timer1優先
+    P2DIR |= 0xC0;              //定?器通道2-3具有第一優先順序
+    P1DIR |= 0x01;				//P1_0為輸出
+    
+    //Timer模式?置
+    T1CTL = 0x02;               //250KHZ不分頻，模模式
+    
+    //根據Table7-1，P1_0必須裝Timer1通道2進行比較
+	
+    T1CCTL2 = 0x1C;             //比較相等為1，計數器回0則清零
+	
+    //裝Timer通道0初值
+    T1CC0H = 0x09;
+    T1CC0L = 0xC4;              //PWM信號週期20ms
+	
+	
+	
+	
+	
+	//?Timer通道2比?值
+    T1CC2H = 0x01;
+    T1CC2L = 0x77; 		//1%的正工作週期
+    //T1CC2L = 0xE1; 	//10%的正工作週期
+    //T1CC2L = 0xC8; 	//20%的正工作週期
+    //T1CC2L = 0xAF; 	//30%的正工作週期
+    //T1CC2L = 0x96; 	//40%的正工作週期
+    //T1CC2L = 0xC4; 	//50%的正工作週期
+    //T1CC2L = 0x64; 	//60%的正工作週期
+    //T1CC2L = 0x4B; 	//70%的正工作週期
+    //T1CC2L = 0x32; 	//80%的正工作週期
+    //T1CC2L = 0x19; 	//90%的正工作週期
+    //T1CC2L = 0x0A; 	//99%的正工作週期
+	
+    //T1CC2L = 0x01; 	//?置通道2比?寄存器初值
+	
 }
 
 void set_main_clock()
@@ -148,4 +90,7 @@ void set_main_clock()
 	CLKCONCMD |= 0X40;			//選擇16MHZ RCOSC為系統時鐘源
 	while(!(CLKCONSTA & 0X40)); //等待時鐘穩定
 	CLKCONCMD &=~0XF8;			//選擇32MHz為主時鐘
+	
+	CLKCONCMD |= 0x38;          //Timer標記輸出為250kHz
 }
+
